b639b3f0039dbb1e2059b7774f9e56bfebdcc698
Andreas Brackwehr <andreas.brackwehr@bvb.de>
2022-08-25 16:27:13 +0200

    prepare first fetch matchlinks

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 import boto3
+import ScraperFC as sfc
+import traceback
+
 
 load_dotenv(".env.local")
 print(os.getenv("region_name"))
@@ -10,11 +13,24 @@ dynamodb = boto3.resource(
     aws_access_key_id=os.getenv("aws_access_key_id"),
     aws_secret_access_key=os.getenv("aws_secret_access_key"),
 )
-table = dynamodb.Table("dev_seasons")
-res = table.scan()
-print(res)
 
-for y in range(2016, 2023):
-    item = {"id": (y - 2013), "year_start": y, "year_end": (y + 1)}
-    print(item)
-    table.put_item(Item=item)
+
+tab_seasons = dynamodb.Table("dev_seasons")
+seasondata = []
+res = tab_seasons.scan()
+for item in res["Items"]:
+    seasondata.append(item)
+print(seasondata)
+
+tab_leagues = dynamodb.Table("dev_leagues")
+leaguedata = []
+res = tab_leagues.scan()
+for item in res["Items"]:
+    leaguedata.append(item)
+print(leaguedata)
+
+scraper = sfc.Understat()
+tab_matchlinks = dynamodb.Table("dev_raw_matchlinks")
+for season in seasondata:
+    for league in leaguedata:
+        print(f"fetch {season['year_start'], league['name']}")
